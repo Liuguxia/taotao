1ca11562cf85bb95631f67eedf59301da299b301
Liuguxia <1102356056@qq.com>
2018-10-09 16:34:32 +0800

    18.10.9新建一个taotao-commons模块 测试到  图片仅上存成功

--- taotao-manager/src/main/java/com/itheima/ManagerApp.java
+++ taotao-manager/src/main/java/com/itheima/ManagerApp.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+/*
+    manager根本不跟图片打交道，图片上传到fastdfs服务器中，manager只是提取它的地址
+ */
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ManagerApp {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApp.class,args);
