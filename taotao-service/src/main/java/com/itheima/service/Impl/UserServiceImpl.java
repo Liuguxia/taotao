8656a43baf37c2717b6a623a2f500d6929235cd3
Liuguxia <1102356056@qq.com>
2018-11-05 09:06:11 +0800

    18.11.5 实现注册  密码md5加密 http://www.taotao.com/page/register.shtml

--- taotao-service/src/main/java/com/itheima/service/Impl/UserServiceImpl.java
+++ taotao-service/src/main/java/com/itheima/service/Impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.itheima.mapper.UserMapper;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 
 import java.util.Date;
 
@@ -27,8 +28,35 @@ public class UserServiceImpl implements UserService {
         user.setCreated(new Date());
         user.setUpdated(new Date());
 
-
+        //使用MD5对密码进行加密
+        String password=user.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        user.setPassword(password);
 
         return userMapper.insert(user);
     }
+
+    
+    //使用MD5对密码进行加密,小测试，也就是demo
+    //md5不可逆，密文不可转换成明文
+    public static void main(String[] args) {
+//        String s="123";
+        //把字符串转化成字节数组
+//        byte[] bytes = s.getBytes();
+//        String md5 = DigestUtils.md5DigestAsHex(bytes);
+//        System.out.println("123字符经 1次md5加密后:" + md5);
+//        for (int i = 0; i < 10; i++) {
+//            s=DigestUtils.md5DigestAsHex(bytes);
+//        }
+//        System.out.println("123字符经11次md5加密后:" + s);
+
+
+
+        String s="123";
+        for (int i = 0; i < 10; i++) {
+            s = DigestUtils.md5DigestAsHex(s.getBytes());
+        }
+        System.out.println(s);
+    }
+
 }
