0d04cfb5eb2292f76b373f8a55c5c88944283884
Liuguxia <1102356056@qq.com>
2018-11-28 15:42:55 +0800

    18.11.28 搜索系统，设置关键字高亮显示 相关路径：http://search.taotao.com/search.html?q=手机

--- taotao-search-service/src/main/java/com/itheima/service/Impl/SearchServiceImpl.java
+++ taotao-search-service/src/main/java/com/itheima/service/Impl/SearchServiceImpl.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SearchServiceImpl implements SearchService {
@@ -40,6 +41,18 @@ public class SearchServiceImpl implements SearchService {
             query.setStart((page-1)*16);//忽略前面的多少条
             query.setRows(16);//每页返回多少条
 
+            //设置高亮显示
+            query.setHighlight(true);//表示要设置高亮
+
+            //设置标题高亮
+            query.addHighlightField("item_title");
+
+            //设置高亮的前缀和后缀
+            query.setHighlightSimplePre("<font color='red'>");
+            query.setHighlightSimplePost("</font>");
+
+
+
             //查询到当前页的数据
             QueryResponse response = solrClient.query(query);
 
@@ -49,6 +62,12 @@ public class SearchServiceImpl implements SearchService {
 
             //总记录数
             long total=results.getNumFound();
+
+            //获取高亮的数据  key:id value:表示高亮的数据
+            //因为高亮的字段可以设置多个，比如设置item_title,item_price,item_name....
+            //接着由于某些域可以存放多个值，所以内部的map的value类型又是一个list集合
+            //也就是说：title可以有多个值，一般只会有一个
+            Map<String, Map<String, List<String>>> map = response.getHighlighting();
 
             //TODO:需要封装当前这一页的数据
             //当前这一页的集合数据
@@ -63,9 +82,20 @@ public class SearchServiceImpl implements SearchService {
 
                 Item item=new Item();
                 item.setId(id);
-                item.setTitle(title);
-                item.setImage(image);
                 item.setCid(cid);
+
+                //这是设置高亮的文字标题
+                List<String> list = map.get(id + "").get("item_title");
+                if (list!=null&&list.size()>0){
+                    String hlTitle = list.get(0);
+                    item.setTitle(hlTitle);
+                }else {
+                    //这是设置普通的文字标题
+                    item.setTitle(title);
+                }
+
+
+                item.setImage(image);
                 item.setPrice(price);
 
                 //遍历一次item，就打印一次item
